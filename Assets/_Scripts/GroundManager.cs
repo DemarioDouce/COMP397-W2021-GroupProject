e2bb5acd5bea2c7fa82acd74a58f226c8cda6161
Demario Douce <demario.douce567@gmail.com>
2021-01-31 23:50:23 -0500

    Generating Infinite Tiles.

--- Assets/_Scripts/GroundManager.cs
+++ Assets/_Scripts/GroundManager.cs
@@ -8,6 +8,9 @@ public class GroundManager : MonoBehaviour
     //Hold the ground prefabs and the array amount.
     public GameObject[] groundPrefabs;
 
+    //Hold current ground
+    private List<GameObject> activeGround = new List<GameObject>();
+
     //Number of time it will spawn on the z axis.
     public float zSpawn = 0;
 
@@ -15,7 +18,10 @@ public class GroundManager : MonoBehaviour
     private float groundLength = 9;
 
     //Number of ground we wanna see on the screen.
-    private int numberOfGround = 50;
+    private int numberOfGround = 10;
+
+    //Get the player
+    public Transform playerTransform;
 
     // Start is called before the first frame update
     void Start()
@@ -38,13 +44,29 @@ public class GroundManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (playerTransform.position.z - 10 > zSpawn - numberOfGround * groundLength) {
+
+            spawnGround(Random.Range(0, groundPrefabs.Length));
+
+            DeleteGround();
+
+        }
         
     }
 
-    //Spawn title method.
+    //Spawn ground method.
     public void spawnGround(int groundIndex) {
 
-        Instantiate(groundPrefabs[groundIndex],transform.forward * zSpawn, transform.rotation);
+       GameObject go = Instantiate(groundPrefabs[groundIndex],transform.forward * zSpawn, transform.rotation);
+        activeGround.Add(go);
         zSpawn += groundLength;
+    }
+
+    //Delete ground.
+    private void DeleteGround() {
+
+        Destroy(activeGround[0]);
+        activeGround.RemoveAt(0);
     }
 }
