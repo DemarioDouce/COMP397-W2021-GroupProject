858ef297515d0e8b1e3b3f888896868f40edfd17
Demario Douce <demario.douce567@gmail.com>
2021-02-07 10:27:40 -0500

    Added game level panel.

---
+++ Assets/_Scripts/GamePlayPanel.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePlayPanel : MonoBehaviour
+{
+    //Ref to gameplay panel bg
+    public GameObject GameplayPanelBg;
+    //Ref to playPause Btn
+    public Text playPauseBtnTxt;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameplayPanelBg.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerManger.isGameStarted == true)
+        {
+            GameplayPanelBg.SetActive(true);
+        }
+        if (PlayerManger.gameOver == true) {
+            GameplayPanelBg.SetActive(false);
+        }
+    }
+
+    public void Pause() {
+
+        if (playPauseBtnTxt.text == "PAUSE")
+        {
+            Time.timeScale = 0;
+            playPauseBtnTxt.text = "PLAY";
+        }
+        else {
+
+            Time.timeScale = 1;
+            playPauseBtnTxt.text = "PAUSE";
+        }
+        
+    }
+}
