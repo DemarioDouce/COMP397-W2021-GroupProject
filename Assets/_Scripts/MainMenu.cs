c35888cfc6f87c7625acb02fb5c6a865daec4c43
Demario Douce <demario.douce567@gmail.com>
2021-02-09 12:00:18 -0500

    Added load game canvas.

--- Assets/_Scripts/MainMenu.cs
+++ Assets/_Scripts/MainMenu.cs
@@ -5,6 +5,8 @@ public class MainMenu : MonoBehaviour
 {
     //Ref to main canvas.
     public GameObject MainCanvas;
+    //Ref to load game canvas.
+    public GameObject LoadGameCanvas;
     //Ref to options canvas.
     public GameObject OptionsCanvas;
     //Start the game method.
@@ -24,9 +26,17 @@ public class MainMenu : MonoBehaviour
         MainCanvas.SetActive(false);
     }
 
+    //Load game 
+    public void LoadGame()
+    {
+        LoadGameCanvas.SetActive(true);
+        MainCanvas.SetActive(false);
+    }
+
     //Go back
     public void GoBack()
     {
+        LoadGameCanvas.SetActive(false);
         OptionsCanvas.SetActive(false);
         MainCanvas.SetActive(true);
     }
