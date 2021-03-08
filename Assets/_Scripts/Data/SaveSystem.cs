5cacee18888e5fa99cdd7f043786164ebb8addaf
Sergio Silva <sergiobtos@hotmail.com>
2021-03-08 18:17:18 -0500

    Update SaveSystem.cs

--- Assets/_Scripts/Data/SaveSystem.cs
+++ Assets/_Scripts/Data/SaveSystem.cs
@@ -6,14 +6,14 @@ using System.Runtime.Serialization.Formatters.Binary;
 public static class SaveSystem 
 {
     public static void SavePlayer(PlayerManger playerManger) {
-        //BinaryFormatter formatter = new BinaryFormatter();
+        BinaryFormatter formatter = new BinaryFormatter();
         string path = Application.persistentDataPath + "/player.json";
-        //FileStream stream = new FileStream(path,FileMode.Create);
+        FileStream stream = new FileStream(path,FileMode.Create);
 
         PlayerData data = new PlayerData(playerManger);
 
-        //formatter.Serialize(stream, data);
-        //stream.Close();
+        formatter.Serialize(stream, data);
+        stream.Close();
         Debug.Log("Path = "+Application.persistentDataPath);
         string json = JsonUtility.ToJson(data);
         StreamWriter sw = File.CreateText(path);
